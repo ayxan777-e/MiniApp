1a612a4c493a403780459c813d380d4441de0bd5
ayxan777-e <eliyevayxan2006@gmail.com>
2026-01-02 11:57:14 +0400

    created DTO and Validation about UpdateRequest

---
+++ MiniApp/DTOs/UpdateReservationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniApp.DTOs;
+
+public class UpdateReservationRequest
+{
+    public int DiningTableId { get; set; }
+    public DateTime ReservationDate { get; set; }
+    public int GuestCount { get; set; }
+    public string CustomerName { get; set; } = null!;
+}
---
+++ MiniApp/Validation/UpdateReservationRequestValidation.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MiniApp.DTOs;
+
+namespace MiniApp.Validation;
+
+public class UpdateReservationRequestValidation:AbstractValidator<UpdateReservationRequest>
+{
+    public UpdateReservationRequestValidation()
+    {
+        RuleFor(x => x.CustomerName)
+            .NotEmpty()
+            .WithMessage("Customer name is required.");
+
+        RuleFor(x => x.GuestCount)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("Guest count must be at least 1.");
+
+        RuleFor(x => x.ReservationDate)
+            .GreaterThan(DateTime.Now)
+            .WithMessage("Reservation date must be in the future.");
+
+        RuleFor(x => x.DiningTableId)
+            .GreaterThan(0)
+            .WithMessage("DiningTableId must be a valid id.");
+    }
+}
